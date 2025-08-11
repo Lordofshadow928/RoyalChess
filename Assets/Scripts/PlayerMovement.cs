843657f2da9abafa8e41eb1c6e309ea8ac73b9f3
Lordofshadow928 <flairdante@gmail.com>
2025-08-11 21:33:25 +0700

    snakeee movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,38 +6,59 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public float steerSpeed = 180.0f;
+    public float bodySpeed = 5.0f;
+    public int gap = 50;
+
     private Rigidbody rb;
     private BoxCollider boxCollider;
     private float horizontalInput;
+    public GameObject bodyPrefab;
+
+    private List<GameObject> bodyParts = new List<GameObject>();
+    private List<Vector3> positionHistory = new List<Vector3>();
     public void Awake()
     {
         rb = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
     }
 
+    public void Start()
+    {
+        // Initialize the snake with a few body parts
+            growSnake();
+            growSnake();
+            growSnake();
+            growSnake();
+    }
     public void Update()
     {
         //Flip();
         //float moveHorizontal = Input.GetAxis("Horizontal");
         //float moveVertical = Input.GetAxis("Vertical");
         //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        float moveVertical = Input.GetAxis("Vertical");
-        transform.position +=transform.forward * moveVertical * moveSpeed * Time.deltaTime;
+        //float movevertical = input.getaxis("vertical");
+        transform.position +=transform.forward * moveSpeed * Time.deltaTime;
         float steerDirection = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * steerDirection * steerSpeed * Time.deltaTime);
+
+        positionHistory.Insert(0, transform.position);
+
+        int index = 0;
+        foreach (var body in bodyParts)
+        {
+            Vector3 point = positionHistory[Mathf.Min(index * gap, positionHistory.Count - 1)];
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+            body.transform.LookAt(point);
+            index++;
+        }
     }
 
-    //private void Flip()
-    //{
-    //    horizontalInput = Input.GetAxis("Horizontal");
-    //    if (horizontalInput > 0.01f)
-    //    {
-    //        transform.localScale = Vector3.one;
-    //    }
-    //    else if (horizontalInput < -0.01f)
-    //    {
-    //        transform.localScale = new Vector3(-1, 1, 1);
-    //    }
-    //}               
+    private void growSnake()
+    {
+        GameObject body = Instantiate(bodyPrefab);
+        bodyParts.Add(body);
+
+    }
 
 }
